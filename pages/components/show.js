48622b24e7733e8f30429194f3b304d885f695f6
austinStotts <austintstotts@gmail.com>
2022-06-10 20:49:07 -0500

    build issues again

--- pages/components/show.js
+++ pages/components/show.js
@@ -3,7 +3,7 @@
 export default function Show({ show, key }) {
   console.log(show.name)
   return (
-    <div key={key} className={"show-wrapper"} key={show.name}>
+    <div key={key} className={"show-wrapper"}>
       <div className="show-title">{show.name}</div>
       <div className="show-studio">{show.studio}</div>
       <div className="show-year">{show.year}</div>
