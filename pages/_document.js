ccd0ea30810197fc77037a3f9f533319ebb980e9
austinStotts <austintstotts@gmail.com>
2022-06-10 20:53:22 -0500

    _document was the problem

--- pages/_document.js
+++ pages/_document.js
@@ -1,15 +1,16 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
-export default () => 
-
-<Html>
-  <Head>
-    <title>SLC</title>
-  </Head>
-  <body>
-    <Main />
-    <NextScript />
-  </body>
-</Html>
-
+export default function doc() { 
+  return (
+    <Html>
+      <Head>
+        <title>SLC</title>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
 
