c490f1c2b9cfe93c8e5e55238238c6828fec0efd
austinStotts <austintstotts@gmail.com>
2022-06-10 22:56:09 -0500

    hmmmmm

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,10 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-
+let kawaii = ['(* ^ ω ^)', '(─‿‿─)', '(っ˘ω˘ς )', '(¬‿¬ )', '(ｏ・_・)ノ”(ノ_<、)', '(」°ロ°)」', '(＞﹏＜)', '( ` ω ´ )', '.･ﾟﾟ･(／ω＼)･ﾟﾟ･.'];
 export default function doc() { 
   return (
     <Html>
       <Head>
-        <title>SLC</title>
+        <title>{`SLC ${kawaii[Math.floor(Math.random() * kawaii.length)]}`}</title>
       </Head>
       <body>
         <Main />
