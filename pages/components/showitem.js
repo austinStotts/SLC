627666a4aade2d91e6d0f685fab0df63dc3be93b
austinStotts <austintstotts@gmail.com>
2022-07-23 12:28:53 -0500

    trying again

--- pages/components/showitem.js
+++ pages/components/showitem.js
@@ -1,10 +1,16 @@
+import { useRouter } from 'next/router';
 
 
 
 const ShowItem = ({ show }) => {
+  const router = useRouter();
+  const toShowPage = () => {
+    router.push(`/show/${show.showid}`);
+  }
+  console.log(show);
   if(show) {
     return (
-      <div className={"show-wrapper"}>
+      <div className="show-wrapper" onClick={toShowPage}>
         <img src={show.promo} className="show-promo"></img>
         <div className="show-info">
           <div className="show-title">{show.name}</div>
