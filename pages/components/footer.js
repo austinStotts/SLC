bd8c3220482ba8857835e6b1efe27aca71969ec3
Austin Stotts <astotts@cdicon.com>
2022-09-30 14:30:18 -0500

    tos and resource link on footer

--- pages/components/footer.js
+++ pages/components/footer.js
@@ -12,7 +12,8 @@ const Footer = (props) => {
       <div className="inner-footer">
         <ul className='footer-list'>
           <a className='version'><li>{`Version: 0.3.1`}</li></a>
-          <a className='author'><li>{`Steve`}</li></a>
+          <a className='author' href='http://austinstotts.com/'><li>{`steve`}</li></a>
+          <a className='resources-a' href='https://stevelovescartoons.com/resources/$'>resources</a>
         </ul>
       </div>
     </div>
--- pages/resources/[$].js
+++ pages/resources/[$].js
@@ -1,10 +1,11 @@
-import PrivacyPolicy from "./privacypolicy";
+
 
 export default function resources () {
     return (
         <div className="resources-list-wrapper">
             <h1>List of Site Resources:</h1>
             <a href="./privacypolicy">privacy policy</a>
+            <a href="./termsofservice">Terms of Service</a>
         </div>
     )
 }
