407ad9b5bbdb2211a303a9889b142df1659250f9
Austin Stotts <astotts@cdicon.com>
2022-12-20 15:43:43 -0600

    added show sorting

--- pages/api/search/[search].js
+++ pages/api/search/[search].js
@@ -26,6 +26,28 @@ export default function handler(req, res) {
           res.status(404);
           client.end();
         })
+      } else if(req.query.search == "all-abc") {
+        client.query(`SELECT * FROM shows ORDER BY showid ASC;`)
+        .then(response => {
+          // console.log(response);
+          res.status(200).json({ "data": response });
+          client.end();
+        }).catch(err => {
+          console.log(err);
+          res.status(404);
+          client.end();
+        })
+      } else if(req.query.search == "all-zyx") {
+        client.query(`SELECT * FROM shows ORDER BY showid DESC;`)
+        .then(response => {
+          // console.log(response);
+          res.status(200).json({ "data": response });
+          client.end();
+        }).catch(err => {
+          console.log(err);
+          res.status(404);
+          client.end();
+        })
       } else {
         client.query(`SELECT * FROM shows WHERE showid LIKE '%${req.query.search}%';`)
         .then(response => {
--- pages/home/homeList.js
+++ pages/home/homeList.js
@@ -9,13 +9,46 @@ class HomeList extends Component {
   constructor () {
     super();
     this.state = {
-      shows: []
+      shows: [],
+      order: "abc"
     }
 
     this.getData = this.getData.bind(this);
+    this.switchOrder = this.switchOrder.bind(this);
+  }
+
+  switchOrder (e, newOrder) {
+    document.getElementById("c1").classList.remove("active");
+    document.getElementById("c2").classList.remove("active");
+    document.getElementById("c3").classList.remove("active");
+    e.target.classList.add("active");
+    this.setState({order: newOrder}, () =>{this.refreshData()});
+  }
+
+  refreshData () {
+    if(this.state.order == "abc") {
+      Axios.get(`/api/search/all-abc`)
+      .then(data => {
+        //console.log("got the stuff");
+        this.setState({ shows: data.data.data.rows });
+      }).catch(err => {console.log(err)});
+    } else if (this.state.order == "zyx") {
+      Axios.get(`/api/search/all-zyx`)
+      .then(data => {
+        //console.log("got the stuff");
+        this.setState({ shows: data.data.data.rows });
+      }).catch(err => {console.log(err)});
+    } else {
+      Axios.get(`/api/search/all`)
+      .then(data => {
+        //console.log("got the stuff");
+        this.setState({ shows: data.data.data.rows });
+      }).catch(err => {console.log(err)});
+    }
   }
 
   getData () {
+    console.log("getting new data!")
     if(!this.state.shows.length > 0) {
       // console.log(this.props.showid)
       Axios.get(`/api/search/all`)
@@ -31,7 +64,14 @@ class HomeList extends Component {
     //console.log(this.state.shows)
     if(this.state.shows.length > 0) {
       return (
+        <div>
+          <center><div className="controls-wrapper">
+            <button id="c1" className="con active default" onClick={(e) => {this.switchOrder(e, "default")}}>favorites</button>
+            <button id="c2" className="con abc" onClick={(e) => {this.switchOrder(e, "abc")}}>abc</button>
+            <button id="c3" className="con zyx" onClick={(e) => {this.switchOrder(e, "zyx")}}>zyx</button>
+          </div></center>
         <center><div className="shows-wrapper">
+
           {this.state.shows.map((show, i) => {
               let showjson = JSON.parse(show.showjson);
               if(showjson.public == "true") {
@@ -40,6 +80,7 @@ class HomeList extends Component {
           })
           }
         </div></center>
+        </div>
       )
     } else {
       return (
